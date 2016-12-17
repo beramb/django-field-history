a86da7adea504cb34486fa0c5cf34a4fc49721e7
Egor Blajenov <egor@MacBook-Pro-mvideo.local>
2016-12-17 02:59:20 +0200

    adjustments in admin panel for fields history

--- field_history/__init__.py
+++ field_history/__init__.py
@@ -1,1 +1,2 @@
 __version__ = '0.5.0'
+default_app_config = 'field_history.apps.DomainsManagementConfig'
---
+++ field_history/apps.py
@@ -0,0 +1,7 @@
+from __future__ import unicode_literals
+from django.apps import AppConfig
+
+
+class DomainsManagementConfig(AppConfig):
+    name = 'field_history'
+    verbose_name = 'Changes History'
